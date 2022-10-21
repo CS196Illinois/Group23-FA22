c646ab7964cd7cfb9c63628c872d24306b459b5e
Hammy1020 <hammy@Hammy.localdomain>
2022-10-21 17:25:40 -0500

    Made some asthetic changes. Title added. In options menu: Shortcuts and languages deleted. In Dungeon: Filled in walls and moved/rescaled boss. Cleaned up some code documentation

--- MVP/Scripts/Options Menu.gd
+++ MVP/Scripts/Options Menu.gd
@@ -1,5 +1,5 @@
 extends MarginContainer
 # Go Back button
-func _on_TextureButton_pressed():
+func _on_Go_Back_pressed():
 	# Goes back to Main Menu
 	get_tree().change_scene("res://Scenes/Main Menu.tscn")
