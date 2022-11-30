bb890f3234bfc13cc6bd7598f756bcd26165a1ad
Hammy1020 <hammy.pa@gmail.com>
2022-11-30 13:53:58 -0600

    Implemented Wesam's parsing code. Waiting on Haozhen's dictionary creation in order to send a dictionary to the dungeon.

--- MVP/Scripts/Import Quizlet.gd
+++ MVP/Scripts/Import Quizlet.gd
@@ -1,20 +1,26 @@
 extends MarginContainer
 
-var description = """This is a test question	this is the answer
-This is another question	this is another answer"""
-var questionAnswer = description.split('\n')
-var question = questionAnswer[0].split('	')
-var question1 = questionAnswer[1].split('	')
+func _on_Back_pressed():
+	get_tree().change_scene("res://Scenes/Set Creator.tscn")\
 
-func _ready():
+#NEED TO MAKE PARSED DATA INTO MAP AND GET MAP TO DUNGEON
+func _on_Import_pressed():
+	#Gets text and parses into an array with Wesam's code
+	var quizletText: String = $"Copy&Paste/Quizlet Text Container/Quizlet Text".get_text()
+	var arr = _parse(quizletText)
+	#TURNS ARRAY INTO A MAP WITH HAOZHEN'S CODE
+	var map = _toMap(arr)
+	get_tree().change_scene("res://Scenes/Dungeon.tscn")
+
+func _parse(text: String):
+	var questionAnswer = text.split('\n')
+	var question = questionAnswer[0].split('	')
+	var question1 = questionAnswer[1].split('	')
 	print(question[0])
 	print(question[1])
 	print(question1[0])
 	print(question1[1])
+	return questionAnswer
 
-func _on_Back_pressed():
-	get_tree().change_scene("res://Scenes/Set Creator.tscn")\
-
-#NEED TO PARSE AND GET DATA TO DUNGEON
-func _on_Import_pressed():
-	get_tree().change_scene("res://Scenes/Dungeon.tscn")
+func _toMap(arr: Array):
+	
